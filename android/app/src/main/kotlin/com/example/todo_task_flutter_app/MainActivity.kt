aaaaf7162e49399b704bbc90557ce6c6706e4896
Mac <ajdourelhassa2096@gmail.com>
2025-05-24 20:34:25 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/todo_task_flutter_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todo_task_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:todo_task_flutter_app/todo_app.dart';
+
+void main() {
+  runApp(TodoApp());
+}
